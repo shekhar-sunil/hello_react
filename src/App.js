30bc5339cd447071a718116b50a40d189f0ec11b
shekharsunil <127013432+shekhar-sunil@users.noreply.github.com>
2023-03-23 08:59:41 -0700

    updated app file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React from Programming with Sunil
+          Learn React from Programming with Sunil Sir
         </a>
       </header>
     </div>
